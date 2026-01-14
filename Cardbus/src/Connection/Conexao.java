710e5b0c3bfdfd207ea8b8584370b868910fea00
Guilherme Olympio <ginguiolympio@gmail.com>
2026-01-14 01:03:59 -0300

    Update Conexao.java

--- Cardbus/src/Connection/Conexao.java
+++ Cardbus/src/Connection/Conexao.java
@@ -2,6 +2,7 @@ package Connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import java.sql.SQLException;
 public class Conexao {
@@ -25,4 +26,6 @@ public class Conexao {
         
         return con;
     }
+    
+    
 }
